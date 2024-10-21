923638282fd8d09c96de7a51c086d79c26d3334b
Demonmasterlqx <183842220@qq.com>
2024-10-21 21:29:54 +0800

    no thing

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
-moon build --target wasm
-npx wasm4 run target/wasm/release/build/GAME.wasm
+@REM moon build --target wasm
+@REM npx wasm4 run target/wasm/release/build/GAME.wasm
+moon build --target wasm -g
+npx wasm4 run target/wasm/debug/build/GAME.wasm
