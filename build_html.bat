3bf557db2deffff71c3f4ef6806e0e7cc8d0bce3
Demonmasterlqx <183842220@qq.com>
2024-11-14 18:02:38 +0800

    no thing

--- build_html.bat
+++ build_html.bat
@@ -1,2 +1,4 @@
-moon build --target wasm
-npx wasm4 bundle --html game.html target/wasm/release/build/GAME.wasm
+@REM moon build --target wasm
+@REM npx wasm4 bundle --html game.html target/wasm/release/build/GAME.wasm
+moon build --source-dir  ./ --target wasm-gc
+npx wasm4 bundle --html target/wasm-gc/release/build/GAME.wasm
