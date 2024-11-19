19763e03de69f2146c22240b7fb4fe31024a0387
Demonmasterlqx <183842220@qq.com>
2024-11-19 08:29:08 +0800

    wsgc

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-@REM moon build --target wasm
-@REM npx wasm4 run target/wasm/release/build/GAME.wasm
+moon build --target wasm
+npx wasm4 run target/wasm/release/build/GAME.wasm
 @REM moon build --target wasm -g
 @REM npx wasm4 run target/wasm/debug/build/GAME.wasm
-moon build --source-dir  ./ --target wasm-gc
-npx wasm4 run target/wasm-gc/release/build/GAME.wasm
+@REM moon build --source-dir  ./ --target wasm-gc
+@REM npx wasm4 run target/wasm-gc/release/build/GAME.wasm
